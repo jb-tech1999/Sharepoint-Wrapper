0f451d66498da54216bd39900af968d5c8841c7a
Jandre <janbadtech@gmail.com>
2023-06-02 13:06:10 +0200

    Added setHeaders and CreateItem function

--- spWrapper.py
+++ spWrapper.py
@@ -5,13 +5,14 @@ import json
 # sharepoint rest wrapper
 class Sharepoint:
     # constructor
-    def __init__(self, client_id, client_secret, tenant_id, resource, grant_type, tenant):
+    def __init__(self, client_id, client_secret, tenant_id, resource, grant_type, tenant, site):
         self.client_id = client_id
         self.client_secret = client_secret
         self.tenant_id = tenant_id
         self.resource = resource
         self.grant_type = grant_type
         self.tenant = tenant
+        self.site = site
 
     # get token
     def getToken(self):
@@ -32,13 +33,27 @@ class Sharepoint:
         self.token = token
     # get lists
 
-    def getLists(self, site, guid, filters):
-        headers = {
+    def setHeaders(self):
+        self.headers = {
             'Authorization': f"Bearer {self.token}",
             'Accept': 'application/json;odata=verbose',
             'Content-Type': 'application/json;odata=verbose'
         }
 
-        list_url = f"https://{self.tenant}.sharepoint.com/sites/{site}/_api/web/lists(guid'{guid}')/Items{filters}"
-        l = requests.get(list_url, headers=headers)
-        return l.text
+    def getLists(self, guid, filters):
+
+        list_url = f"https://{self.tenant}.sharepoint.com/sites/{self.site}/_api/web/lists(guid'{guid}')/Items{filters}"
+        l = requests.get(list_url, headers=self.headers)
+        return l.json()['d']['results']
+
+    def GetEntityname(self):
+        entityFullname = f"https://{self.tenant}.sharepoint.com/sites/{self.site}/_api/web/lists(guid'eb5f9aef-7f94-47c5-a6d9-ce6d56d2ce82')/listItemEntityTypeFullName"
+        entityResponse = requests.get(entityFullname, headers=self.headers)
+        return entityResponse.json()['d']['ListItemEntityTypeFullName']
+
+    def CreateItem(self, guid, data):
+
+        list_url = f"https://{self.tenant}.sharepoint.com/sites/{self.site}/_api/web/lists(guid'{guid}')/Items"
+        l = requests.post(list_url, headers=self.headers,
+                          data=json.dumps(data))
+        return l.json()
